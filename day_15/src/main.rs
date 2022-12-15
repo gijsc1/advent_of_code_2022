56da596a084a07ae815282445b78178b1139e277
Gijs van Cuyck <gijsc1@live.nl>
2022-12-15 17:54:30 +0100

    slight changes to day 15

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -13,39 +13,14 @@ mod layout;
 mod diag_iterator;
 
 fn main() {
-    let sensor_iter = get_lines("input.txt")
+    let sensor_iter = get_lines("extratestinput.txt")
         .map(|l|Sensor::from_str(l.as_str()).unwrap());
     let sensors:Vec<Sensor> = sensor_iter.collect();
 
 
-    // answer1(&sensors);
-    let max_x = 4000000;
+    answer1(&sensors);
+    answer2(&sensors);
 
-     'outerloop: for sensor in sensors.iter(){
-         'coordloop: for bordercoord in DiagIterator::new(sensor.get_location(),sensor.get_distance()+1){
-             if bordercoord.0>max_x || bordercoord.1 > max_x || bordercoord.0<0 || bordercoord.1<0{
-                 continue
-             }
-             // if bordercoord.0==14 {
-             //     println!("debug: now looking at {},{}",bordercoord.0,bordercoord.1);
-             //
-             // }
-
-             for othersensor in sensors.iter(){
-                if !othersensor.can_contain_beacon(bordercoord){
-                    // if bordercoord.0==14 && bordercoord.1==11 {
-                    //     println!("debug:{},{} is covered by {}", bordercoord.0, bordercoord.1, othersensor);
-                    //     println!("debug:sensor distance:{}, sensor-point distance:{}",othersensor.get_distance(),manhatten_dist(othersensor.get_location(),bordercoord));
-                    //     println!("doublecheck contains:{}",othersensor.debug_can_contain_beacon(bordercoord));
-                    // }
-                    continue 'coordloop;
-                }
-            }
-             let frequency:u128 = (bordercoord.0 as u128)*4000000 +bordercoord.1 as u128;
-             println!("answer2: coord: {},{}. freq:{}",bordercoord.0,bordercoord.1,frequency);
-             break 'outerloop;
-        }
-    }
     println!("Hello, world!");
 }
 
@@ -67,4 +42,34 @@ fn answer1(sensors:&Vec<Sensor>){
         }
     }
     println!("answer1:{counter}");
+}
+
+fn answer2(sensors:&Vec<Sensor>){
+    let max_x = 4000000;
+
+    'outerloop: for sensor in sensors.iter(){
+        'coordloop: for bordercoord in DiagIterator::new(sensor.get_location(),sensor.get_distance()+1){
+            if bordercoord.0>max_x || bordercoord.1 > max_x || bordercoord.0<0 || bordercoord.1<0{
+                continue
+            }
+            // if bordercoord.0==14 {
+            //     println!("debug: now looking at {},{}",bordercoord.0,bordercoord.1);
+            //
+            // }
+
+            for othersensor in sensors.iter(){
+                if !othersensor.can_contain_beacon(bordercoord){
+                    // if bordercoord.0==14 && bordercoord.1==11 {
+                    //     println!("debug:{},{} is covered by {}", bordercoord.0, bordercoord.1, othersensor);
+                    //     println!("debug:sensor distance:{}, sensor-point distance:{}",othersensor.get_distance(),manhatten_dist(othersensor.get_location(),bordercoord));
+                    //     println!("doublecheck contains:{}",othersensor.debug_can_contain_beacon(bordercoord));
+                    // }
+                    continue 'coordloop;
+                }
+            }
+            let frequency:u128 = (bordercoord.0 as u128)*4000000 +bordercoord.1 as u128;
+            println!("answer2: coord: {},{}. freq:{}",bordercoord.0,bordercoord.1,frequency);
+            break 'outerloop;
+        }
+    }
 }
